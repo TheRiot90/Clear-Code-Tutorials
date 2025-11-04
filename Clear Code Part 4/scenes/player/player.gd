3243b96fc42f5a112debb664b7a77145d63b2874
Joey <j.theriot3@gmail.com>
2025-11-03 19:51:15 -0600

    finished with hoe and watering can features

--- Clear Code Part 4/scenes/player/player.gd
+++ Clear Code Part 4/scenes/player/player.gd
@@ -4,9 +4,11 @@ extends CharacterBody2D
 @onready var move_state_machine: AnimationNodeStateMachinePlayback = $AnimationTree.get("parameters/MoveStateMachine/playback")
 @onready var tool_state_machine: AnimationNodeStateMachinePlayback = $AnimationTree.get("parameters/ToolStateMachine/playback")
 var direction: Vector2
+var last_direction: Vector2
 var speed := 300
 var can_move := true
-
+@export var tool_direction_offset := 20
+@export var tool_y_offset := 4
 enum Tools {HOE, AXE, WATER}
 var current_tool: Tools = Tools.AXE
 const tool_connection = {
@@ -14,11 +16,14 @@ const tool_connection = {
 	Tools.AXE: 	'axe',
 	Tools.WATER:	'water',
 }
+signal tool_use(tool: Tools, pos: Vector2)
 
 
 func _physics_process(_delta: float) -> void:
 	if can_move:
 		get_input()
+	if direction:
+		last_direction = direction
 	velocity = direction * speed * int(can_move)
 	move_and_slide()
 	animation()
@@ -31,6 +36,9 @@ func get_input():
 		tool_state_machine.travel(tool_connection[current_tool])
 		$AnimationTree.set('parameters/OneShot/request', AnimationNodeOneShot.ONE_SHOT_REQUEST_FIRE)
 		can_move = false
+		if current_tool in [Tools.HOE, Tools.WATER]:
+			await $AnimationTree.animation_finished
+			tool_use.emit(current_tool, position + last_direction * tool_direction_offset + Vector2(0,tool_y_offset))
 
 
 	if Input.is_action_just_pressed('tool_forward') or Input.is_action_just_pressed('tool_backward'):
